e5a2b455016337b8fbcdb215679fd146ab6b1984
Leonardo Martins <martins.moreira.leonardo@escola.pr.gov.br>
2023-10-07 17:18:10 -0300

    Trabalho de bosta que eu fiz usando chat e o professo

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import Cart from "./components/Cart"
-import ListaProdutos from "./components/ListaProdutos"
-import { useState } from "react";
-import { Produto } from './components/ListaProdutos'
+import Cart from "./components/Cart";
+import ListaProdutos from "./components/ListaProdutos";
+import { useState, useEffect } from "react";
+import { Produto } from "./components/ListaProdutos";
 
 function App() {
   const [cart, setCart] = useState<Produto[]>([]);
   const addCarrinho = (produto: Produto) => () => {
-    setCart([...cart, produto])
+    setCart([...cart, produto]);
+  };
 
-}
+  useEffect(() => {
+    if (cart.length > 0) {
+      console.log("Produto adicionado ao carrinho");
+    }
+  }, [cart]);
 
   return (
-  <>
-  <ListaProdutos
-  addCarrinho={addCarrinho}
-  />
-  <Cart
-  cartProducts={cart}
-  />
- </>
-  )
+    <>
+      <ListaProdutos addCarrinho={addCarrinho} />
+      <Cart cartProducts={cart} />
+    </>
+  );
 }
 
-export default App
+export default App;
